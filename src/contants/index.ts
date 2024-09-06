7f95717da3db1295184ad3ae1c1bb4a466a52ad8
aysegulsimsek <aysegulsimsek999@gmail.com>
2024-09-06 23:33:48 +0300

    first commit

---
+++ src/contants/index.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { href: "/#home", label: "Home" },
+    { href: "/#about-us", label: "Benefits" },
+    { href: "/#products", label: "Our Classes" },
+    { href: "/#contact-us", label: "Contact Us" },
+];
